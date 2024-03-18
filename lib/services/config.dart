ca4baa4e289acc2e96a9d50eb4554fae71d04b45
Malcolm Mason Rodriguez <malcolmjmr@gmail.com>
2024-03-18 10:01:43 -0400

    .

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,5 +1,3 @@
 class ServicesConfig {
-  static const openAiApiKey = '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';
-  static const mistralApiKey = 'lLLHoSt1DloR4UniKHcr7HG0rERFSFhv';
-  static const braveApiKey = 'BSA3WK0JWMOjS4ObBq_jpbE4KEhlwGq';
+
 }
